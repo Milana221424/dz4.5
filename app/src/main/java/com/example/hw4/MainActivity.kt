5c566e94c68458f7691297ca29c109112e7baf9b
ZhannaSap <nekotyan474@gmail.com>
2023-11-30 11:12:33 +0600

    added sheredPreference and onBoarding with Module

--- app/src/main/java/com/example/hw4/MainActivity.kt
+++ app/src/main/java/com/example/hw4/MainActivity.kt
@@ -4,33 +4,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.example.hw4.data.local.Pref
 import com.example.hw4.databinding.ActivityMainBinding
 import com.example.hw4.remote.LoveModel
 import com.example.hw4.remote.RetrofitService
+import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    @Inject
+    lateinit var pref: Pref
+    private lateinit var binding: ActivityMainBinding
+    private var navController: NavController? = null
 
-    lateinit var binding: ActivityMainBinding
-    private val viewModel: LoveViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initClickers()
-    }
 
-    private fun initClickers() {
-        with(binding) {
-            btnGetLove.setOnClickListener {
-                viewModel.getLiveLoveData(etFirstName.text.toString(), etSecondName.text.toString())
-                    .observe(this@MainActivity, Observer {
-                        tvResult.text = it.toString()
-                    })
-            }
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        if (!pref.isShow()) {
+            navController?.navigate(R.id.onBoardingFragment)
         }
     }
+
+
 }
